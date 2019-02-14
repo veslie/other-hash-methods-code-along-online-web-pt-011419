c85bfae1763f820610424f1ef26698ac287519b4
veslie losius <veslielosius@yahoo.com>
2019-02-14 03:17:47 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -1,16 +1,5 @@
 require 'pry'
 
-groceries = {
- dairy: ["milk", "yogurt", "cheese"],
- vegetable: ["carrots", "broccoli", "cucumbers"],
-meat: ["chicken", "steak", "salmon"],
- grains: ["rice", "pasta"]
-}
-
-def get_the_min(groceries)
-  groceries.values.flatt.en.min 
-end
- 
  groceries = {
   dairy: ["milk", "yogurt", "cheese"],
   vegetable: ["carrots", "broccoli", "cucumbers"],
