3c993980b0a39fba194a8c76d4d275ba78ef3243
veslie losius <veslielosius@yahoo.com>
2019-02-14 02:57:44 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,6 +1,11 @@
 require 'pry'
 
-
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
 
 def get_the_values(groceries)
   #code your solution here!
